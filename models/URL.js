82741861dc61fcde82d67f05283eba3d9efb3dd6
AggieWB <usmaggie23@gmail.com>
2020-08-09 11:49:18 -0700

    Update model to require mongoose-auto-increment node
    module, and pass the module into the mongoose connection and the plugin() function on the URL schema where the short-url is defined as the field to increment by 1 starting at 1
    Set useFindAndModify to false on the mongoose.connection to fix deprecation warning

--- models/URL.js
+++ models/URL.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
 
 mongoose.connection.on('open', () => console.log('Mongoose connected.'));
 mongoose.connection.on('error', err => console.log(`Mongoose could not connect: ${err}`));
@@ -12,8 +13,12 @@ const urlSchema = mongoose.Schema({
 });
 
 //TODO: increment short url here
-urlSchema.pre('save', next => {
-
+autoIncrement.initialize(mongoose.connection);
+urlSchema.plugin(autoIncrement.plugin, {
+    model: 'URL',
+    field: 'short_url',
+    startAt: 1,
+    incrementBy: 1
 });
 
-module.exports = mongoose.model('URL', urlSchema);
+module.exports = mongoose.model('shortenurl', urlSchema);
